a4086435305deab0c03a721cb8ebb9f4c9fc1b74
dhawaksir <dhawaksir@gmail.com>
2023-07-08 06:50:45 +0545

    commit error in footer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,61 +8,51 @@ import Blog from "./components/Blog/Blog";
 import ContactUs from "./components/Contact/Contact";
 import Services from "./components/services/Services";
 import Project from "./components/Project/Project";
-
-
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <Router>
-      <div>
+      <div className="app">
         <div className="parallax" />
 
-        <div className="header">
-        <img src={logo} alt="NEPEROX" className="logo"  />
+        <header className="header">
+          <img src={logo} alt="NEPEROX" className="logo" />
 
-
-          <ul className="navigation">
-            <li>
-              <Link to="#home">Home</Link>
-            </li>
-            <li>
-              <Link to="/About">About Us</Link>
-            </li>
-            <li>
-              <a href="/blog">Blog</a>
-            </li>
-            <li>
-              <Link to="/contact">contact Us</Link>
-            </li>
-            <li>
-              <a href="/Services">Services</a>
-            </li>
-            <li>
-              <a href="/Project">Projects</a>
-            </li>
-          </ul>
-        </div>
+          <nav className="navigation">
+            
+              <li>
+                <Link to="/Home">Home</Link>
+              </li>
+              <li>
+                <Link to="/About">About Us</Link>
+              </li>
+              <li>
+                <Link to="/Blog">Blog</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact Us</Link>
+              </li>
+              <li>
+                <Link to="/Services">Services</Link>
+              </li>
+              <li>
+                <Link to="/Project">Projects</Link>
+              </li>
+       
+          </nav>
+        </header>
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/About" element={<About />} />
-          <Route path="/Blog" element={<Blog/>} />
+          <Route path="/Blog" element={<Blog />} />
           <Route path="/contact" element={<ContactUs />} />
-          <Route path="/Services" element={<Services/>} />
-          <Route path="/project" element={<Project/>} />
-          <Route path="/footer" element={<footer/>} />
-
-         
-          
+          <Route path="/Services" element={<Services />} />
+          <Route path="/Project" element={<Project />} />
         </Routes>
 
-       
-       
-
-      
-
-
-        
+        <Footer />
       </div>
     </Router>
   );
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,111 +1,70 @@
-// import React from "react";
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FaFacebookF, FaTwitter, FaYoutube } from "react-icons/fa";
+import "./footer.css";
+// import logo from "./src/assets/logo.png";
 
-// import { Container,Row,Col } from 'react-bootstrap'
-// import { Link } from "react-router-dom";
-// import "./footer.css";
+const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
 
-// const quickLinks = [
-//   {
-//     path: "/about",
-//     display: "About",
-//   },
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="4" md="4" sm="12">
+            <div>
+              {/* <img src={logo} alt="NEPEROX" className="logo" /> */}
+              <h1>
+                <Link to="/home" className="d-flex align-items-center gap-2">
+                  <i className=""></i>
+                  <h6>GET IN TOUCH</h6>
+                </Link>
+              </h1>
+              <div className="footer__logo-content">
+                <div className="social-media-icons">
+                  <h3>Follow Us</h3>
+                  <a href="https://www.facebook.com/example" target="_blank" rel="noopener noreferrer">
+                    <FaFacebookF size={32} />
+                  </a>
+                  <a href="https://www.twitter.com/example" target="_blank" rel="noopener noreferrer">
+                    <FaTwitter size={32} />
+                  </a>
+                  <a href="https://www.youtube.com/example" target="_blank" rel="noopener noreferrer">
+                    <FaYoutube size={32} />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </Col>
 
-//   {
-//     path: "#",
-//     display: "Privacy Policy",
-//   },
+          <Col lg="8" md="8" sm="12">
+            <Row>
+              <Col lg="6" md="6" sm="6">
+                <div className="mb-4">
+                  <h5 className="footer__link-title mb-4">Office</h5>
+                  <p className="office__info">Rupendehi, Butwal, Divertole</p>
+                  <p className="office__info">Phone: +0995345875365</p>
+                  <p className="office__info">teams.alpha1@gmail.com</p>
+                  <p className="office__info">Office Time: 12pm - 6pm</p>
+                </div>
+              </Col>
 
-//   {
-//     path: "/services",
-//     display: "service",
-//   },
-//   {
-//     path: "/blogs",
-//     display: "Blog",
-//   },
+              <Col lg="6" md="6" sm="6">
+                <div className="footer__bottom">
+                  <p className="section__description d-flex align-items-center justify-content-center gap-1 pt-4">
+                    <i className="ri-copyright-line"></i>
+                    Copyright {year}, Developed by neperox. All rights reserved.
+                  </p>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
 
-//   {
-//     path: "/contact",
-//     display: "Contact",
-//   },
-// ];
-
-// const Footer = () => {
-//   const date = new Date();
-//   const year = date.getFullYear();
-//   return (
-//     <footer className="footer">
-//       <Container>
-//         <Row>
-//           <Col lg="4" md="4" sm="12">
-//             <div className="logo footer__logo">
-//               <h1>
-//                 <Link to="/home" className=" d-flex align-items-center gap-2">
-//                   <i class=""></i>
-//                   <span>
-//                     NEPEROX<br /> Service
-//                   </span>
-//                 </Link>
-//               </h1>
-//             </div>
-//             <p className="footer__logo-content">
-//               Lorem ipsum dolor sit amet consectetur adipisicing elit.
-//               Consequuntur, distinctio, itaque reiciendis ab cupiditate harum ex
-//               quam veniam, omnis expedita animi quibusdam obcaecati mollitia?
-//               Delectus et ad illo recusandae temporibus?
-//             </p>
-//           </Col>
-
-//           {/* <Col lg="2" md="4" sm="6">
-//             <div className="mb-4">
-//               <h5 className="footer__link-title">Quick Links</h5>
-//               <ListGroup>
-//                 {quickLinks.map((item, index) => (
-//                   <ListGroupItem key={index} className="p-0 mt-3 quick__link">
-//                     <Link to={item.path}>{item.display}</Link>
-//                   </ListGroupItem>
-//                 ))}
-//               </ListGroup>
-//             </div>
-//           </Col> */}
-
-//           <Col lg="3" md="4" sm="6">
-//             <div className="mb-4">
-//               <h5 className="footer__link-title mb-4">Office</h5>
-//               <p className="office__info">Rupendehi,Butwal,Divertole</p>
-//               <p className="office__info">Phone: +0995345875365</p>
-
-//               <p className="office__info">teams.alpha1@gmail.com</p>
-
-//               <p className="office__info">Office Time: 12pm - 6pm</p>
-//             </div>
-//           </Col>
-
-//           <Col lg="3" md="4" sm="12">
-//             <div className="mb-4">
-//               <h5 className="footer__link-title">Newsletter</h5>
-//               <p className="section__description">Subscribe our newsletter</p>
-//               <div className="newsletter">
-//                 <input type="email" placeholder="Email" />
-//                 <span>
-//                   <i class="ri-send-plane-line"></i>
-//                 </span>
-//               </div>
-//             </div>
-//           </Col>
-
-//           <Col lg="12">
-//             <div className="footer__bottom">
-//               <p className="section__description d-flex align-items-center justify-content-center gap-1 pt-4">
-//                 <i class="ri-copyright-line"></i>Copyright {year}, Developed by
-//                 neperox. All rights reserved.
-//               </p>
-//             </div>
-//           </Col>
-//         </Row>
-//       </Container>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
+export default Footer;
