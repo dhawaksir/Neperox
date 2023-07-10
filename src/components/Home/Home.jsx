e61015c530993909446db20bcef6af7dac12e534
dhawaksir <dhawaksir@gmail.com>
2023-07-10 22:36:51 +0545

    refress for git

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div id="home" className="section">
       <div className="section-content">
-        <h1>Welcome to NEPEROX IT Services</h1>
+        <h1>Welcome to NEPEROX </h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
           vestibulum enim ac urna blandit, at cursus velit dignissim. Duis
