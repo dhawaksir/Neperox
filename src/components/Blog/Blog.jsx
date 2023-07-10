2aeb47dfdfbc8109a4a7f39322ccb076a91edbca
dhawaksir <dhawaksir@gmail.com>
2023-07-10 22:53:46 +0545

    blog part

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,35 +1,74 @@
-// import React, { useEffect, useState } from 'react';
-// // import axios from 'axios';
+import React from "react";
+// import "./blog.css"
+// import Blog1 from './img/blog-01.jpg';
+// import Blog2 from './img/blog-02.jpg';
+// import Blog3 from './img/blog-03.jpg';
 
-// const Blog = () => {
-//   const [blogPosts, setBlogPosts] = useState([]);
+function Blog () {
+    return (
+        <div className="blog component__space" id="Blog">
+            <div className="heading">
+                <h1 className="heading">Latest News</h1>
+                <p className="heading p__color">There are many variations of passages of
+                Lorem Ipsum available,</p>
+                <p className="heading p__color">but the majority have suffered alteration</p>
+            </div>
 
-//   useEffect(() => {
-//     fetchBlogPosts();
-//   }, []);
+            <div className="container">
+                <div className="row">
 
-//   const fetchBlogPosts = async () => {
-//     try {
-//       const response = await axios.get('/api/blogposts');
-//       setBlogPosts(response.data);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
+                     <div className="col__3">
+                        <div className="project__box pointer relative">
+                            <div className="porject__box__img pointer relative">
+                                <div className="project__img__box">
+                                    {/* <img src={Blog1} alt="" className="project__img" /> */}
+                                </div>
+                                <div className="mask__effect"></div>
+                            </div>
+                            <div className="Blog__meta absolute">
+                                <h5 className="project__text">Development</h5>
+                                <h4 className="project__text">Getting tickets to the big show</h4>
+                                <a href="button" className="blog project__btn btn">Read More</a>
+                            </div>
+                        </div>
+                     </div>
 
-//   return (
-//     <div>
-//       <h1>Blog</h1>
-//       {blogPosts.map((post) => (
-//         <div key={post._id}>
-//           <h2>{post.title}</h2>
-//           <p>{post.content}</p>
-//           <p>Author: {post.author}</p>
-//           <p>Date: {post.createdAt}</p>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
+                     <div className="col__3">
+                        <div className="project__box pointer relative">
+                            <div className="porject__box__img pointer relative">
+                                <div className="project__img__box">
+                                    {/* <img src={Blog2} alt="" className="project__img" /> */}
+                                </div>
+                                <div className="mask__effect"></div>
+                            </div>
+                            <div className="Blog__meta absolute">
+                                <h5 className="project__text">Development</h5>
+                                <h4 className="project__text">Getting tickets to the big show</h4>
+                                <a href="button" className="blog project__btn btn">Read More</a>
+                            </div>
+                        </div>
+                     </div>
 
-// export default Blog;
+                     <div className="col__3">
+                        <div className="project__box pointer relative">
+                            <div className="porject__box__img pointer relative">
+                                <div className="project__img__box">
+                                    {/* <img src={Blog3} alt="" className="project__img" /> */}
+                                </div>
+                                <div className="mask__effect"></div>
+                            </div>
+                            <div className="Blog__meta absolute">
+                                <h5 className="project__text">Development</h5>
+                                <h4 className="project__text">Getting tickets to the big show</h4>
+                                <a href="button" className="blog project__btn btn">Read More</a>
+                            </div>
+                        </div>
+                     </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Blog;
