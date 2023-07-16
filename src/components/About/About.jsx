d4221fb870abb918cc90a8da533d6943af35f588
dhawaksir <dhawaksir@gmail.com>
2023-07-16 22:00:53 +0545

    Your commit message

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,104 +1,105 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
-// import "../../styles/our-member.css";
-import "./about.css";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import './about.css';
 
-// import ava01 from "../../assets/all-images/ava-1.jpg";
-// import ava02 from "../../assets/all-images/ava-2.jpg";
-// import ava03 from "../../assets/all-images/ava-3.jpg";
+const AboutPage = () => {
+  const message = `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.`;
 
-const OUR_MEMBERS = [
-  {
-    name: "Jhon Doe",
-    experience: "5 years of experience",
-    fbUrl: "#",
-    instUrl: "#",
-    twitUrl: "#",
-    linkedinUrl: "#",
-    // imgUrl: ava01,
-  },
-  {
-    name: "David Lisa",
-    experience: "5 years of experience",
-    fbUrl: "#",
-    instUrl: "#",
-    twitUrl: "#",
-    linkedinUrl: "#",
-    // imgUrl: ava02,
-  },
-  {
-    name: "Hilton King",
-    experience: "5 years of experience",
-    fbUrl: "#",
-    instUrl: "#",
-    twitUrl: "#",
-    linkedinUrl: "#",
-    // imgUrl: ava03,
-  },
-  {
-    name: "Jhon Doe",
-    experience: "5 years of experience",
-    fbUrl: "#",
-    instUrl: "#",
-    twitUrl: "#",
-    linkedinUrl: "#",
-    // imgUrl: ava01,
-  },
-];
+  const teamMembers = [
+    {
+      name: 'Johnathan Hawkins',
+      position: 'Head of SEO',
+      description: 'Johnathan is our co-founder and has developed search strategies for a variety of clients from international brands to medium-sized businesses for over five years.',
+      imageUrl: 'https://demo.epic-webdesign.com/tf-pacifico/v1/images/team1a.jpg',
+      socialMedia: {
+        twitter: '#',
+        pinterest: '#',
+        facebook: '#',
+        dribbble: '#',
+      },
+    },
+    {
+      name: 'Alexandra Smiths',
+      position: 'SEO Specialist',
+      description: 'Graduating with a degree in Spanish and English, Alexandra has always loved writing, and now she’s lucky enough to do it as part of her new job inside our agency.',
+      imageUrl: 'https://demo.epic-webdesign.com/tf-pacifico/v1/images/team2a.jpg',
+      socialMedia: {
+        twitter: '#',
+        pinterest: '#',
+        facebook: '#',
+        dribbble: '#',
+      },
+    },
+    {
+      name: 'Elisa Johanson',
+      position: 'Marketing Manager',
+      description: 'Elisa first fell in love with digital marketing at the university. She loves to learn and looks forward to being part of this new exciting industry for many years.',
+      imageUrl: 'https://demo.epic-webdesign.com/tf-pacifico/v1/images/team3a.jpg',
+      socialMedia: {
+        twitter: '#',
+        pinterest: '#',
+        facebook: '#',
+        dribbble: '#',
+      },
+    },
+  ];
 
-const OurMembers = () => {
   return (
-    <>
-      {OUR_MEMBERS.map((item, index) => (
-        <Col lg="3" md="3" sm="4" xs="6" key={index} className="mb-4">
-          <div className="single__member">
-            <div className="single__member-img">
-              <img src={item.imgUrl} alt="" className="w-100" />
-
-              <div className="single__member-social">
-                <Link to={item.fbUrl}>
-                  <i className="ri-facebook-line"></i>
-                </Link>
-                <Link to={item.instUrl}>
-                  <i className="ri-instagram-line"></i>
-                </Link>
-              </div>
-            </div>
-
-            <h6 className="text-center mb-0 mt-3">{item.name}</h6>
-            <p className="section__description text-center">
-              {item.experience}
-            </p>
-          </div>
-        </Col>
-      ))}
-    </>
-  );
-};
-
-const About = () => {
-  return (
-    <Container>
-      <Row className="mb-5 mt-3">
-        <Col lg="12">
-          <h1 className="display-4 mb-4">About Us </h1>
-          <hr className="t_border my-4 ml-0 text-left" />
-        </Col>
-      </Row>
-      <section className="section-white">
+    <div className="about-page">
+      <Container>
         <Row className="mb-5 mt-3">
           <Col lg="12">
-            <h2 className="section-title text-center">Testimonials</h2>
+            <h1 className="display-4 mb-4">About Us</h1>
+            <hr className="t_border my-4 ml-0 text-left" />
           </Col>
         </Row>
-
-        <Row className="team-row">
-          <OurMembers />
-        </Row>
-      </section>
-    </Container>
+        <section className="section-white">
+          <Row className="mb-5 mt-3">
+            <Col lg="12">
+              <h2 className="section-title text-center">The Team Behind Pacifico</h2>
+              <p className="section-subtitle">{message}</p>
+            </Col>
+          </Row>
+          <Row className="team-row">
+            {teamMembers.map((member, index) => (
+              <Col sm="4" md="4" key={index}>
+                <div className="team-item">
+                  <img src={member.imageUrl} className="team-img" alt="pic" />
+                  <h3>{member.name}</h3>
+                  <div className="team-info">
+                    <p>{member.position}</p>
+                  </div>
+                  <p>{member.description}</p>
+                  <ul className="team-icon">
+                    <li>
+                      <a href={member.socialMedia.twitter} className="twitter">
+                        <i className="fa fa-twitter"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href={member.socialMedia.pinterest} className="pinterest">
+                        <i className="fa fa-pinterest"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href={member.socialMedia.facebook} className="facebook">
+                        <i className="fa fa-facebook"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href={member.socialMedia.dribbble} className="dribble">
+                        <i className="fa fa-dribbble"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </section>
+      </Container>
+    </div>
   );
 };
 
-export default About;
+export default AboutPage;
